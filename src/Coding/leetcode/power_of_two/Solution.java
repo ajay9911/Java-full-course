449060f312d88a4bed0aa32df495b5404716725b
Ajay Kumar <akay99116280@gmail.com>
2025-02-06 22:47:03 +0530

    Add project files

---
+++ src/Coding/leetcode/power_of_two/Solution.java
@@ -0,0 +1,26 @@
+package Coding.leetcode.power_of_two;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        boolean b = isPowerOfTwo(9);
+        System.out.println(b);
+
+    }
+
+    public static boolean isPowerOfTwo(int n){
+        if (n<0){
+            return false;
+        }
+
+        while (n>1){
+            if (n%2!=0){
+                return false;
+            }
+            n=n/2;
+        }
+
+
+        return true;
+    }
+}
